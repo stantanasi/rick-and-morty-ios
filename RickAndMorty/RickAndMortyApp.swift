4e6a456b5aa2fd601c6b8575d3364e0367ffc496
Lory-Stan TANASI <stantanasi@gmail.com>
2023-07-10 17:04:02 +0200

    Initial commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Lory-Stan TANASI on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
