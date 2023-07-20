02d6e1580738fc2804652327f98db5516deab91c
Lory-Stan TANASI <stantanasi@gmail.com>
2023-07-20 17:30:44 +0200

    feat(character): display character's image

--- RickAndMorty/View/Character/CharacterView.swift
+++ RickAndMorty/View/Character/CharacterView.swift
@@ -18,8 +18,20 @@ struct CharacterView: View {
     
     var body: some View {
         List {
+            Section(
+                header: Text("Appearance"),
+                content: {
+                    HStack {
+                        Spacer()
+                        AsyncImage(url: URL(string: viewModel.character?.image ?? ""))
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                        Spacer()
+                    }
+                }
+            )
         }
         .navigationTitle(viewModel.character?.name ?? "Loading...")
+        .listStyle(GroupedListStyle())
         .task {
             await viewModel.getCharacter(id: self.id)
         }
