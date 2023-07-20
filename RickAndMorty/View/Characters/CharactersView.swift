f0af880f78b6c1180a7f9b02a0fd43304985db1a
Lory-Stan TANASI <stantanasi@gmail.com>
2023-07-20 15:39:17 +0200

    feat(characters): call view model inside view

--- RickAndMorty/View/Characters/CharactersView.swift
+++ RickAndMorty/View/Characters/CharactersView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct CharactersView: View {
+    
+    @StateObject private var viewModel = CharactersViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
