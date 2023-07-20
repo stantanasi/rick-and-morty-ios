6d5916b82c1685a36b3f755bb050916b56c1b835
Lory-Stan TANASI <stantanasi@gmail.com>
2023-07-20 14:29:39 +0200

    feat: create characters view

---
+++ RickAndMorty/View/Characters/CharactersView.swift
@@ -0,0 +1,20 @@
+//
+//  CharactersView.swift
+//  RickAndMorty
+//
+//  Created by Lory-Stan TANASI on 20/07/2023.
+//
+
+import SwiftUI
+
+struct CharactersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharactersView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharactersView()
+    }
+}
