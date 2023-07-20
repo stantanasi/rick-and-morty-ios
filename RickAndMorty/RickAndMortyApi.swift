476f40a1671e1b873964a9cfba949d5f8bd3613d
Lory-Stan TANASI <stantanasi@gmail.com>
2023-07-20 11:18:32 +0200

    feat(api): implement retrieving all characters

--- RickAndMorty/RickAndMortyApi.swift
+++ RickAndMorty/RickAndMortyApi.swift
@@ -12,6 +12,17 @@ class RickAndMortyApi {
     let baseUrl = "https://rickandmortyapi.com/api"
     
     
+    func getAllCharacters() async throws -> ResponseInfo<Character> {
+        let url = URL(string: "\(baseUrl)/character")!
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let decoded = try JSONDecoder().decode(ResponseInfo<Character>.self, from: data)
+        
+        return decoded
+    }
+    
+    
     struct ResponseInfo<Element: Decodable>: Decodable {
         let info: Info
         let results: [Element]
