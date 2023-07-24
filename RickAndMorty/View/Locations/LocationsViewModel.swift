3737a77156f8f6d126f6d203b707dd9170960907
Lory-Stan TANASI <stantanasi@gmail.com>
2023-07-24 17:18:58 +0200

    feat(locations): implement retrieving locations

--- RickAndMorty/View/Locations/LocationsViewModel.swift
+++ RickAndMorty/View/Locations/LocationsViewModel.swift
@@ -8,4 +8,22 @@
 import Foundation
 
 class LocationsViewModel: ObservableObject {
+    
+    private let rickAndMortyApi = RickAndMortyApi()
+    
+    @Published public var locations: [Location] = []
+    
+    init() {
+        getLocations()
+    }
+    
+    
+    func getLocations(page: Int = 1) {
+        Task {
+            let response = try! await rickAndMortyApi.getAllLocations(page: page)
+            DispatchQueue.main.async {
+                self.locations.append(contentsOf: response.results)
+            }
+        }
+    }
 }
