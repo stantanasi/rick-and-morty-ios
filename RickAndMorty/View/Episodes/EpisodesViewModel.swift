c93bc54d894dc8216d2aa8dd7a9f4ff443529960
Lory-Stan TANASI <stantanasi@gmail.com>
2023-07-24 17:01:39 +0200

    feat(episodes): implement retrieving episodes

--- RickAndMorty/View/Episodes/EpisodesViewModel.swift
+++ RickAndMorty/View/Episodes/EpisodesViewModel.swift
@@ -8,4 +8,22 @@
 import Foundation
 
 class EpisodesViewModel: ObservableObject {
+    
+    private let rickAndMortyApi = RickAndMortyApi()
+    
+    @Published public var episodes: [Episode] = []
+    
+    init() {
+        getEpisodes()
+    }
+    
+    
+    func getEpisodes(page: Int = 1) {
+        Task {
+            let response = try! await rickAndMortyApi.getAllEpisodes(page: page)
+            DispatchQueue.main.async {
+                self.episodes.append(contentsOf: response.results)
+            }
+        }
+    }
 }
