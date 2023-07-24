3fa7416cec00f92772b7d55df223ae0ce3989e01
Lory-Stan TANASI <stantanasi@gmail.com>
2023-07-24 16:59:09 +0200

    feat: create episodes view

---
+++ RickAndMorty/View/Episodes/EpisodesView.swift
@@ -0,0 +1,20 @@
+//
+//  EpisodesView.swift
+//  RickAndMorty
+//
+//  Created by Lory-Stan TANASI on 24/07/2023.
+//
+
+import SwiftUI
+
+struct EpisodesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EpisodesView_Previews: PreviewProvider {
+    static var previews: some View {
+        EpisodesView()
+    }
+}
