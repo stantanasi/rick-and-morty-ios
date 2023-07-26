51c4c9f167c33406eccce2a852322c8ed3bfb5e9
Lory-Stan TANASI <stantanasi@gmail.com>
2023-07-26 15:52:58 +0200

    feat: create location view

---
+++ RickAndMorty/View/Location/LocationView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationView.swift
+//  RickAndMorty
+//
+//  Created by Lory-Stan TANASI on 26/07/2023.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationView()
+    }
+}
